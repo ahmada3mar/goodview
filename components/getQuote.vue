d5d56e286cc6da5586ae273fd904bf1310358dee
awais <awais.dev321@gmail.com>
2025-08-27 21:08:51 +0500

    changes

--- components/getQuote.vue
+++ components/getQuote.vue
@@ -10,8 +10,8 @@
                 <p class="text-lg md:text-xl text-black text-center mb-6">
                     Get in touch with us for a personalized moving quote and experience a smooth relocation experience.
                 </p>
-                <UButton label="Free Moving Quote" to="/contact" size="xl"
-                    class="px-3 bg-black md:px-7 rounded-[10px]   font-jakarta hover:bg-primary-500 hover:text-slate-300 flex justify-center items-center mx-auto  text-white font-bold"" />
+                <UButton label="Free Moving Quote" to="/quote" size="xl"
+                    class="px-3 bg-black md:px-7 rounded-[10px] font-jakarta hover:bg-primary-500 hover:text-slate-300 flex justify-center items-center mx-auto text-white font-bold" />
             </div>
         </div>
     </div>
